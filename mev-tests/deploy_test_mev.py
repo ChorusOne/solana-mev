81f7aa2cefccb3058fc9628e7eb6c6cc1298efb2
thalita <thalita@chorus.one>
2022-08-30 19:42:59 -0300

    Creates tokens to setup Orca pools

---
+++ mev-tests/deploy_test_mev.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2021 Chorus One AG
+# SPDX-License-Identifier: GPL-3.0
+
+"""
+Set up a Orca instance on local testnet and test logging tx,
+and arb opportunities. 
+"""
+
+import os
+import subprocess
+
+from uuid import uuid4
+
+from util import (
+    create_test_account,
+    rpc_get_account_info,
+    solana,
+    solana_program_deploy,
+    spl_token,
+)
+
+orca_program_id = '9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP'
+# https://solscan.io/account/9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP
+
+deploy_path = '/Users/thalita/solana-private/mev-tests/target/deploy'
+
+# Create a fresh directory where we store all the keys and configuration for this
+# deployment.
+run_id = uuid4().hex[:10]
+test_dir = f'mev-tests/.keys/{run_id}'
+os.makedirs(test_dir, exist_ok=True)
+print(f'Keys directory: {test_dir}')
+
+# Before we start, check our current balance. We also do this at the end,
+# and then we know how much the deployment cost.
+sol_balance_pre = float(solana('balance').split(' ')[0])
+
+# If the Orca program exists, use that, otherwise upload it at a new address.
+amm_info = rpc_get_account_info(orca_program_id)
+if amm_info is not None:
+    print('\nFound existing instance of Orca Token Swap program.')
+    token_swap_program_id = orca_program_id
+else:
+    print('\nUploading Orca Token Swap program ...')
+    # first run: 
+    # solana program dump ORCA_PROGRAM_ID 'path/orca_token_swap_v2.so'
+    token_swap_program_id = solana_program_deploy(
+        deploy_path + '/orca_token_swap_v2.so'
+    )
+print(f'> Token swap program id is {token_swap_program_id}')
+
+# Next up is the token pool, but to be able to set that up,
+# we need tokens and then we need to put that in some new accounts
+# that the pool will take ownership of.
+t0_mint_keypair = create_test_account(f'{test_dir}/token0-mint.json', fund=False)
+spl_token('create-token', f'{test_dir}/token0-mint.json', '--decimals', '6')
+token0_mint_address = t0_mint_keypair.pubkey
+
+try:
+    t0_account_info_json = spl_token(
+        'create-account', token0_mint_address, '--output', 'json'
+    )
+except subprocess.CalledProcessError:
+    # "spl-token create-account" fails if the associated token account exists
+    # already. It would be nice to check whether it exists before we try to
+    # create it, but unfortunately there appears to be no way to get the address
+    # of the associated token account, either through the Solana RPC, or through
+    # one of the command-line tools. The associated token account address remains
+    # implicit everywhere :/
+    pass
+
+t1_mint_keypair = create_test_account(f'{test_dir}/token1-mint.json', fund=False)
+spl_token('create-token', f'{test_dir}/token1-mint.json', '--decimals', '6')
+token1_mint_address = t1_mint_keypair.pubkey
+
+try:
+    t1_account_info_json = spl_token(
+        'create-account', token1_mint_address, '--output', 'json'
+    )
+except subprocess.CalledProcessError:
+    # "spl-token create-account" fails if the associated token account exists
+    # already. It would be nice to check whether it exists before we try to
+    # create it, but unfortunately there appears to be no way to get the address
+    # of the associated token account, either through the Solana RPC, or through
+    # one of the command-line tools. The associated token account address remains
+    # implicit everywhere :/
+    pass
+
+print('\nSetting up RAY-ORCA pool ...')
+
+# pool accounts and transfer tokens
+pool_orca_keypair = create_test_account(f'{test_dir}/pool-orca.json', fund=False)
+pool_ray_keypair = create_test_account(f'{test_dir}/pool-ray.json', fund=False)
+spl_token('create-account', token0_mint_address, pool_orca_keypair.keypair_path)
+spl_token('create-account', token1_mint_address, pool_ray_keypair.keypair_path)
+spl_token('transfer', token0_mint_address, '0.1', pool_orca_keypair.pubkey)
+spl_token('transfer', token1_mint_address, '0.1', pool_ray_keypair.pubkey)
---
+++ mev-tests/util.py
@@ -0,0 +1,232 @@
+# SPDX-FileCopyrightText: 2021 Chorus One AG
+# SPDX-License-Identifier: GPL-3.0
+
+"""
+Utilities that help writing tests, mainly for invoking programs.
+"""
+
+import json
+import os.path
+import subprocess
+import sys
+
+from urllib import request
+from uuid import uuid4
+
+from typing import List, NamedTuple, Any, Optional, Callable, Dict, Tuple
+
+MAX_VALIDATION_COMMISSION_PERCENTAGE = 5
+
+
+class TestAccount(NamedTuple):
+    pubkey: str
+    keypair_path: str
+
+    def __repr__(self) -> str:
+        return self.pubkey
+
+
+def run(*args: str) -> str:
+    """
+    Run a program, ensure it exits with code 0, return its stdout.
+    """
+    try:
+        result = subprocess.run(args, check=True, capture_output=True, encoding='utf-8')
+
+    except subprocess.CalledProcessError as err:
+        # If a test fails, it is helpful to print stdout and stderr here, but
+        # we don't print them by default because some calls are expected to
+        # fail, and we don't want to pollute the output in that case, because
+        # a log full of errors makes it difficult to locate the actual error in
+        # the noise.
+        if '--verbose' in sys.argv:
+            print('Command failed:', ' '.join(args))
+            print('Stdout:', err.stdout)
+            print('Stderr:', err.stderr)
+        raise
+
+    return result.stdout
+
+
+def get_network() -> str:
+    network = os.getenv('NETWORK')
+    if network is None:
+        return 'http://127.0.0.1:8899'
+    else:
+        return network
+
+
+def solana(*args: str) -> str:
+    """
+    Run 'solana' against network.
+    """
+    return run('solana', '--url', get_network(), '--commitment', 'confirmed', *args)
+
+
+def spl_token(*args: str) -> str:
+    """
+    Run 'spl_token' against network.
+    """
+    return run('spl-token', '--url', get_network(), *args)
+
+
+class SplTokenBalance(NamedTuple):
+    # The raw amount is the amount in the smallest denomination of the token
+    # (i.e. the number of Lamports for wrapped SOL), the UI amount is a float
+    # of `amount_raw` divided by `10^num_decimals`.
+    balance_raw: int
+    balance_ui: float
+
+
+def spl_token_balance(address: str) -> SplTokenBalance:
+    """
+    Return the balance of an SPL token account.
+    """
+    result = run(
+        'spl-token',
+        '--url',
+        get_network(),
+        'balance',
+        '--address',
+        address,
+        '--output',
+        'json',
+    )
+    data: Dict[str, Any] = json.loads(result)
+    amount_raw = int(data['amount'])
+    amount_ui: float = data['uiAmount']
+    return SplTokenBalance(amount_raw, amount_ui)
+
+
+def solana_program_deploy(fname: str) -> str:
+    """
+    Deploy a .so file, return its program id.
+    """
+    assert os.path.isfile(fname)
+    result = solana('program', 'deploy', '--output', 'json', fname)
+    program_id: str = json.loads(result)['programId']
+    return program_id
+
+
+class SolanaProgramInfo(NamedTuple):
+    program_id: str
+    owner: str
+    program_data_address: str
+    upgrade_authority: str
+    last_deploy_slot: int
+    data_len: int
+
+
+def solana_program_show(program_id: str) -> SolanaProgramInfo:
+    """
+    Return information about a program.,
+    """
+    result = solana('program', 'show', '--output', 'json', program_id)
+    data: Dict[str, Any] = json.loads(result)
+    return SolanaProgramInfo(
+        program_id=data['programId'],
+        owner=data['owner'],
+        program_data_address=data['programdataAddress'],
+        upgrade_authority=data['authority'],
+        last_deploy_slot=data['lastDeploySlot'],
+        data_len=data['dataLen'],
+    )
+
+
+def create_test_account(keypair_fname: str, *, fund: bool = True) -> TestAccount:
+    """
+    Generate a key pair, fund the account with 1 SOL, and return its public key.
+    """
+    run(
+        'solana-keygen',
+        'new',
+        '--no-bip39-passphrase',
+        '--force',
+        '--silent',
+        '--outfile',
+        keypair_fname,
+    )
+    pubkey = run('solana-keygen', 'pubkey', keypair_fname).strip()
+    if fund:
+        solana('transfer', '--allow-unfunded-recipient', pubkey, '1.0')
+    return TestAccount(pubkey, keypair_fname)
+
+
+def create_spl_token_account(owner_keypair_fname: str, minter: str) -> str:
+    """
+    Creates an spl token for the given minter
+    spl_token command returns 'Creating account <address>
+             Signature: <tx-signature>'
+    This function returns <address>
+    """
+    return (
+        spl_token('create-account', minter, '--owner', owner_keypair_fname)
+        .split('\n')[0]
+        .split(' ')[2]
+    )
+
+
+def create_test_accounts(*, num_accounts: int) -> List[TestAccount]:
+    result = []
+
+    for i in range(num_accounts):
+        fname = f'test-key-{i + 1}.json'
+        test_account = create_test_account(fname)
+        result.append(test_account)
+
+    return result
+
+
+def wait_for_slots(slots: int) -> None:
+    import time
+
+    """
+    Blocks waiting until `slots` slots have passed.
+    """
+    slots_beginning = int(solana('get-slot'))
+    while True:
+        # Wait 1 second to poll next slot height (around 2 slots)
+        time.sleep(1)
+        elapsed_slots = int(solana('get-slot')) - slots_beginning
+        if elapsed_slots >= slots:
+            break
+
+
+def solana_rpc(method: str, params: List[Any]) -> Any:
+    """
+    Make a Solana RPC call.
+
+    This function is very sloppy, doesn't do proper error handling, and is not
+    suitable for serious use, but for tests or checking things on devnet it's
+    useful.
+    """
+    body = {
+        'jsonrpc': '2.0',
+        'id': str(uuid4()),
+        'method': method,
+        'params': params,
+    }
+    req = request.Request(
+        get_network(),
+        method='POST',
+        data=json.dumps(body).encode('utf-8'),
+        headers={
+            'Content-Type': 'application/json',
+        },
+    )
+    response = request.urlopen(req)
+    return json.load(response)
+
+
+def rpc_get_account_info(address: str) -> Optional[Dict[str, Any]]:
+    """
+    Call getAccountInfo, see https://docs.solana.com/developing/clients/jsonrpc-api#getaccountinfo.
+    """
+    result: Dict[str, Any] = solana_rpc(
+        method='getAccountInfo',
+        params=[address, {'encoding': 'jsonParsed'}],
+    )
+    # The value is either an object with decoded account info, or None, if the
+    # account does not exist.
+    account_info: Optional[Dict[str, Any]] = result['result']['value']
+    return account_info
