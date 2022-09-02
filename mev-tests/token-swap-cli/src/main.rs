bc6c223f6902fd518001e159d63bb9ef42521f8d
Fynn <me@enriquefynn.com>
2022-09-02 13:06:24 -0300

    Implement token swaps in client

--- mev-tests/token-swap-cli/src/main.rs
+++ mev-tests/token-swap-cli/src/main.rs
@@ -1,10 +1,12 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use solana_client::rpc_client::RpcClient;
 use solana_program::pubkey::Pubkey;
 use solana_sdk::{commitment_config::CommitmentConfig, signature::read_keypair_file};
 use utils::{create_token_pool, get_default_keypair_path};
+
+use crate::utils::swap_tokens;
 
 mod utils;
 
@@ -25,6 +27,12 @@ pub struct Opts {
     #[clap(long)]
     token_b_account: Pubkey,
 
+    #[clap(subcommand)]
+    subcommand: OptSubcommand,
+}
+
+#[derive(Parser, Debug)]
+struct InitializeTokenSwap {
     #[clap(long, default_value = "0")]
     trade_fee_numerator: u64,
     #[clap(long, default_value = "100")]
@@ -43,32 +51,81 @@ pub struct Opts {
     host_fee_denominator: u64,
 }
 
+#[derive(Parser, Debug)]
+struct SwapTokens {
+    #[clap(long)]
+    token_swap_program_id: Pubkey,
+    #[clap(long)]
+    token_swap_account: Pubkey,
+    #[clap(long)]
+    token_a_client: Pubkey,
+    #[clap(long)]
+    token_swap_a_account: Pubkey,
+    #[clap(long)]
+    token_swap_b_account: Pubkey,
+    #[clap(long)]
+    token_b_client: Pubkey,
+    #[clap(long)]
+    pool_mint: Pubkey,
+    #[clap(long)]
+    pool_fee: Pubkey,
+    #[clap(long)]
+    amount: u64,
+    #[clap(long)]
+    minimum_amount_out: u64,
+}
+
+#[derive(Debug, Subcommand)]
+enum OptSubcommand {
+    Init(InitializeTokenSwap),
+    Swap(SwapTokens),
+}
+
 fn main() {
     let opts = Opts::parse();
     let signer_path = opts.signer_path.unwrap_or(get_default_keypair_path());
     let rpc_client =
         RpcClient::new_with_commitment(opts.cluster.clone(), CommitmentConfig::confirmed());
-
-    let fees = spl_token_swap::curve::fees::Fees {
-        trade_fee_numerator: opts.trade_fee_numerator,
-        trade_fee_denominator: opts.trade_fee_denominator,
-        owner_trade_fee_numerator: opts.owner_trade_fee_numerator,
-        owner_trade_fee_denominator: opts.owner_trade_fee_denominator,
-        owner_withdraw_fee_numerator: opts.owner_withdraw_fee_numerator,
-        owner_withdraw_fee_denominator: opts.owner_withdraw_fee_denominator,
-        host_fee_numerator: opts.host_fee_numerator,
-        host_fee_denominator: opts.host_fee_denominator,
-    };
-
     let signer_keypair = read_keypair_file(signer_path).unwrap();
 
-    let token_pool = create_token_pool(
-        &rpc_client,
-        &signer_keypair,
-        &opts.token_swap_program_id,
-        &opts.token_a_account,
-        &opts.token_b_account,
-        fees,
-    );
-    println!("{}", serde_json::to_string(&token_pool).unwrap());
+    match opts.subcommand {
+        OptSubcommand::Init(init_opts) => {
+            let fees = spl_token_swap::curve::fees::Fees {
+                trade_fee_numerator: init_opts.trade_fee_numerator,
+                trade_fee_denominator: init_opts.trade_fee_denominator,
+                owner_trade_fee_numerator: init_opts.owner_trade_fee_numerator,
+                owner_trade_fee_denominator: init_opts.owner_trade_fee_denominator,
+                owner_withdraw_fee_numerator: init_opts.owner_withdraw_fee_numerator,
+                owner_withdraw_fee_denominator: init_opts.owner_withdraw_fee_denominator,
+                host_fee_numerator: init_opts.host_fee_numerator,
+                host_fee_denominator: init_opts.host_fee_denominator,
+            };
+
+            let token_pool = create_token_pool(
+                &rpc_client,
+                &signer_keypair,
+                &opts.token_swap_program_id,
+                &opts.token_a_account,
+                &opts.token_b_account,
+                fees,
+            );
+            println!("{}", serde_json::to_string(&token_pool).unwrap());
+        }
+        OptSubcommand::Swap(swap_opts) => {
+            swap_tokens(
+                &rpc_client,
+                &signer_keypair,
+                &swap_opts.token_swap_program_id,
+                &swap_opts.token_swap_account,
+                &swap_opts.token_a_client,
+                &swap_opts.token_swap_a_account,
+                &swap_opts.token_swap_b_account,
+                &swap_opts.token_b_client,
+                &swap_opts.pool_mint,
+                &swap_opts.pool_fee,
+                swap_opts.amount,
+                swap_opts.minimum_amount_out,
+            );
+        }
+    }
 }
