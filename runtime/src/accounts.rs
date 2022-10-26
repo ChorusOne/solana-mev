f7d23e2a7335f8248a3d20f3ec8b16358d0f04ca
Enrique Fynn <fynn@chorus.one>
2022-10-26 16:52:43 +0200

    Update comment
    Co-authored-by: Ruud van Asseldonk <ruud@chorus.one>

--- runtime/src/accounts.rs
+++ runtime/src/accounts.rs
@@ -132,7 +132,7 @@ pub struct MevPoolAccounts {
     pub pool_mint: TransactionAccount,
     pub pool_fee: TransactionAccount,
     // `pool_authority` is an authority derived from the `pool` and `token_id`.
-    // Newer versions of the Token Swap program does not require this field.
+    // Newer versions of the Token Swap program do not require this field.
     pub pool_authority: TransactionAccount,
 }
 
